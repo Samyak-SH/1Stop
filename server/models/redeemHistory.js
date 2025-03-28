411ba1ff31ff44400c95f29179d3d0c72c919fd9
ishaansaxena25 <saxenaishaan2004@gmail.com>
2025-03-28 17:44:17 +0530

    schemas

---
+++ server/models/redeemHistory.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const redeemHistorySchema = new mongoose.Schema({
+  uid: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  trans_id: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  type: {
+    type: String,
+    enum: ["bus", "metro", "electricity", "recycling_reward"],
+    required: true,
+  },
+  redeemedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const RedeemHistoryModel = mongoose.model("RedeemHistory", redeemHistorySchema);
+
+module.exports = RedeemHistoryModel;
--- server/models/userModel.js
+++
@@ -1,9 +0,0 @@
-const {mongoose} = require("../utils/util.js");
-
-const userSchema = new mongoose.Schema({
-    //ishaaaan kardeee
-});
-
-// const userModel = ///aage karle
-
-// module.exports = { userModel }; ye bhi karle
